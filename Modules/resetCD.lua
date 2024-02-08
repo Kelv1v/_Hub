355f23f40d31a7b18d41cb56cdc70a3cc3f8ac7d
BadTime <155080154+Kelv1v@users.noreply.github.com>
2024-02-09 00:40:25 +0700

    Create resetCD.lua

---
+++ Modules/resetCD.lua
@@ -0,0 +1,14 @@
+local caidai={}
+function caidai:resetCD(Controller,fucker,RigEvent)
+    local WeaponName = Controller.currentWeaponModel.Name
+    local Cooldown = {
+        combat = 0.07
+    }
+    AttackCD = tick() + (fucker and Cooldown[WeaponName:lower()] or fucker or 0.285) + ((TryLag/MaxLag)*0.3)
+    RigEvent.FireServer(RigEvent,"weaponChange",WeaponName)
+    TryLag += 1
+    task.delay((fucker or 0.285) + (TryLag+0.5/MaxLag)*0.3,function()
+        TryLag -= 1
+    end)
+end
+return caidai
