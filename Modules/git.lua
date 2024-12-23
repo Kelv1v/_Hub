75d3aaa9aa9ecbbf5c3f44e0122c4e2730bfbb71
BadTime <155080154+Kelv1v@users.noreply.github.com>
2024-12-24 00:39:44 +0700

    Update git.lua

--- Modules/git.lua
+++ Modules/git.lua
@@ -107,7 +107,7 @@ Module.FastAttack = (function()
 
 	task.spawn(function()
 		while task.wait(Settings.ClickDelay or 0.125) do
-			if (tick() - Module.AttackCooldown) < 0.37 then continue end
+			if (tick() - Module.AttackCooldown) < 0.3 then continue end
 			if not Settings.AutoClick then continue end
 			if not Module.IsAlive(client.Character) then continue end
 			if not Usefastattack then continue end
